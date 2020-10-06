a63a3d51876d5d67eecbc2def2ef78287a384cde
JayAreP <43276115+JayAreP@users.noreply.github.com>
2020-10-05 18:32:53 -0700

    Add-CNode function corrected

--- FLEX/Public/Add-FLEXClusterCloudCNode.ps1
+++ FLEX/Public/Add-FLEXClusterCloudCNode.ps1
@@ -7,28 +7,21 @@ function Add-FLEXClusterCloudCNode {
     )
 
     begin {
-        $endpoint = 'add_and_install_cloud_nodes'
+        $endpoint = 'add_and_install_cloud_nodes' 
         $api = 'v1'
     }
 
     process {
-
-        $o = New-Object PSObject
+        $o = New-Object psobject
         $o | Add-Member -MemberType NoteProperty -Name "cluster_id" -Value $id
-        $o | Add-Member -MemberType NoteProperty -Name "node_type" -Value "cnode"
-        $o | Add-Member -MemberType NoteProperty -Name "cloud_node_type" -Value "ProductionV1"
-        # $nodeIndex = query nodes and add +1 for node_index tally
-        $o | Add-Member -MemberType NoteProperty -Name "node_index" -Value 
-        $nodeId = 'flex-cluster-' + $id + '-' + $nodeType + '-' + $nodeIndex
-        $o | Add-Member -MemberType NoteProperty -Name "node_id" -Value $nodeId
-        # query node size somehow to add friendly name
-        $o | Add-Member -MemberType NoteProperty -Name "friendly_name" -Value $null
+        $o | Add-Member -MemberType NoteProperty -Name "cloud_node_type" -Value "Production"
+        $o | Add-Member -MemberType NoteProperty -Name 'friendly_name' -Value '64 vCPU 256GB'
 
-        $results = Invoke-FLEXRestCall -method POST -API $api -endpoint $endpoint -body $o -flexContext $flexContext
+        $body = New-Object psobject 
+        $body | Add-Member -MemberType NoteProperty -Name 'cnodes' -Value @($o)
+        $body | Add-Member -MemberType NoteProperty -Name 'mnodes' -Value @()
 
-        return $results
+        Invoke-FLEXRestCall -method POST -endpoint $endpoint -API $api -body $body
     }
-
-
-
 }
+
