802f27f687a2adc2bb4cf8b63cec720e7e53e062
JayAreP <43276115+JayAreP@users.noreply.github.com>
2022-02-16 12:56:52 -0700

    tasks query updates

--- FLEX/Public/Core/Get-FLEXTask.ps1
+++ FLEX/Public/Core/Get-FLEXTask.ps1
@@ -21,7 +21,32 @@ function Get-FLEXTask {
         }
     
         $results = Invoke-FLEXRestCall -method GET -API $api -endpoint $endpoint
-        $results = $results.hits._obj
+
+        # Parsing this kinda stupid considering I could have written a class for this much more easily. Will do just that later. 
+
+        $resultArray = @()
+        if ($results.hits) {
+            foreach ($r in $results.hits) {
+                $o = New-Object psobject
+                $o | Add-Member -MemberType NoteProperty -Name "taskid" -Value $r._id
+                $propArray = ($r._obj | Get-Member -MemberType NoteProperty).name
+                foreach ($n in $propArray) {
+                    $o | Add-Member -MemberType NoteProperty -Name $n -Value $r._obj.$n
+                }
+                $resultArray += $o
+            }
+        } else {
+            $r = $results
+            $o = New-Object psobject
+            $o | Add-Member -MemberType NoteProperty -Name "taskid" -Value $r._id
+            $propArray = ($r._obj | Get-Member -MemberType NoteProperty).name
+            foreach ($n in $propArray) {
+                $o | Add-Member -MemberType NoteProperty -Name $n -Value $r._obj.$n
+            }
+            $resultArray += $o
+        }
+
+        $results = $resultArray
     
         if ($id) {
             $results = $results | Where-Object {$_.plot.node_id -eq $id}
@@ -31,7 +56,6 @@ function Get-FLEXTask {
             $results = $results.steps
         }
 
-        $results = $results | Sort-Object update_ts_millis
         if ($latest) {
             return $results[-1]
         } else {
